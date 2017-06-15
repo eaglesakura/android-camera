1c09f6529ac0c60f3ca482de57d0cee4d4a35b7b
eaglesakura <eagle.sakura@gmail.com>
2017-06-15 11:01:49 +0900

    テストショットを端末から削除するように修正

--- script/collect-testshots.sh
+++ script/collect-testshots.sh
@@ -3,10 +3,14 @@
 rm -rf private/camera/junit
 
 # run android tests
-adb shell am instrument -w -r -e debug false com.eaglesakura.android.camera.test/android.support.test.runner.AndroidJUnitRunner
+#adb shell am instrument -w -r -e debug false com.eaglesakura.android.camera.test/android.support.test.runner.AndroidJUnitRunner
+
+# pull test shots
 adb pull /sdcard/Android/data/com.eaglesakura.android.camera.test/files/shot/ private/camera/junit
 
 for path in `find private/camera/junit -name "*.jpg"` ; do
     echo $path
     cp $path private/camera//images
 done
+
+adb shell rm -rf /sdcard/Android/data/com.eaglesakura.android.camera.test/files/shot/
